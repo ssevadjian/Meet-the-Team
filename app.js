de17963046cf3d35fce3e76af9f7773533c31eed
SJS <sjsevadjian@gmail.com>
2020-09-23 18:44:55 -0700

    unwrapped most of the functions, and took a.title out of my functions' parameters

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const Employee = require("./lib/Employee");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+const render = require("./lib/htmlRenderer");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
@@ -9,143 +10,144 @@ const fs = require("fs");
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-const render = require("./lib/htmlRenderer");
-
 const myTeam = [];
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 function app() {
-
-function buildTeam() {
-    if (!fs.existsSync(OUTPUT_DIR)) {
-        fs.mkdirSync(OUTPUT_DIR);
-    }
-    fs.writeFileSync(outputPath, render(myTeam), "utf-8");
+  createManager();
 }
 
+function buildTeam() {
+    console.log(myTeam);
+  if (!fs.existsSync(OUTPUT_DIR)) {
+    fs.mkdirSync(OUTPUT_DIR);
+  }
+  fs.writeFileSync(outputPath, render(myTeam), "utf-8");
+}
 
 const addEmployee = () => {
-    inquirer
+  inquirer
     .prompt([
-        {
-            type: "list",
-            message: "What is the employee's job title?",
-            name: "title",
-            choices: [
-                "Engineer", //take out manager as option, because we start as a manager
-                "Intern",
-                "I don't want to add any other team members"
-            ]
-        }
-    ]).then(a => {
-        let employee = new Employee (a.title);
-        if(a.title === "Engineer") {
-            newEngineer();
-        }
-        if(a.title === "Intern") {
-            newIntern();
-        }
-        else {
-            buildTeam();
-            return;
-        }
-        myTeam.push(employee);
+      {
+        type: "list",
+        message: "What is the employee's job title?",
+        name: "title",
+        choices: [
+          "Engineer",
+          "Intern",
+          "I don't want to add any other team members",
+        ],
+      },
+    ])
+    .then((a) => {
+      let employee = new Employee(a.title);
+      if (a.title === "Engineer") {
+        newEngineer();
+      }
+      if (a.title === "Intern") {
+        newIntern();
+      } else {
+        buildTeam();
+        return;
+      }
+      myTeam.push(employee);
     });
 };
 
-
 const newEngineer = () => {
-    inquirer
+  inquirer
     .prompt([
-        {
-            type: "input",
-            message: "Enter first name of employee:",
-            name: "name"
-        },
-        {
-            type: "input",
-            message: "Enter the employee ID:",
-            name: "id"
-        },
-        {
-            type: "input",
-            message: "What is the employee's email address?",
-            name: "email"
-        },
-        {
-            type: "input",
-            message: "What is the employee's GitHub username?",
-            name: "gitUser"
-        }
-    ]).then(a => {
-        let engineer = new Engineer (a.name, a.title, a.id, a.email, a.gitUser);
-        myTeam.push(engineer);
-        addEmployee();
+      {
+        type: "input",
+        message: "Enter first name of employee:",
+        name: "name",
+      },
+      {
+        type: "input",
+        message: "Enter the employee ID:",
+        name: "id",
+      },
+      {
+        type: "input",
+        message: "What is the employee's email address?",
+        name: "email",
+      },
+      {
+        type: "input",
+        message: "What is the employee's GitHub username?",
+        name: "gitUser",
+      },
+    ])
+    .then((a) => {
+      let engineer = new Engineer(a.name, a.id, a.email, a.gitUser);
+      myTeam.push(engineer);
+      addEmployee();
     });
 };
 
 const createManager = () => {
-    inquirer
+  inquirer
     .prompt([
-        {
-            type: "input",
-            message: "Enter first name of manager:",
-            name: "name"
-        },
-        {
-            type: "input",
-            message: "Enter the manager's employee ID:",
-            name: "id"
-        },
-        {
-            type: "input",
-            message: "What is the manager's email address?",
-            name: "email"
-        },
-        {
-            type: "input",
-            message: "What is the manager's contact number?",
-            name: "phone"
-        }
-    ]).then(a => {
-        let manager = new Manager (a.name, a.title, a.id, a.email, a.phone);
-        myTeam.push(manager);
-        addEmployee();
+      {
+        type: "input",
+        message: "Enter first name of manager:",
+        name: "name",
+      },
+      {
+        type: "input",
+        message: "Enter the manager's employee ID:",
+        name: "id",
+      },
+      {
+        type: "input",
+        message: "What is the manager's email address?",
+        name: "email",
+      },
+      {
+        type: "input",
+        message: "What is the manager's contact number?",
+        name: "phone",
+      },
+    ])
+    .then((a) => {
+      let manager = new Manager(a.name, a.id, a.email, a.phone);
+      myTeam.push(manager);
+      addEmployee();
     });
 };
 
 const newIntern = () => {
-    inquirer
+  inquirer
     .prompt([
-        {
-            type: "input",
-            message: "Enter first name of intern:",
-            name: "name"
-        },
-        {
-            type: "input",
-            message: "Enter the internee's ID:",
-            name: "id"
-        },
-        {
-            type: "input",
-            message: "What is the intern's email address?",
-            name: "email"
-        },
-        {
-            type: "input",
-            message: "What school did the intern attend?",
-            name: "school"
-        }
-    ]).then(a => {
-        let intern = new Intern (a.name, a.title, a.id, a.email, a.school);
-        myTeam.push(intern);
-        addEmployee();
+      {
+        type: "input",
+        message: "Enter first name of intern:",
+        name: "name",
+      },
+      {
+        type: "input",
+        message: "Enter the internee's ID:",
+        name: "id",
+      },
+      {
+        type: "input",
+        message: "What is the intern's email address?",
+        name: "email",
+      },
+      {
+        type: "input",
+        message: "What school did the intern attend?",
+        name: "school",
+      },
+    ])
+    .then((a) => {
+      let intern = new Intern(a.name, a.id, a.email, a.school);
+      myTeam.push(intern);
+      addEmployee();
     });
 };
-createManager();
-};
+
 app();
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
